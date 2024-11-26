a757ed19d18a693ce92091bb7c8e59d4fb946e9f
Ryosuke <m1741508@gmail.com>
2024-11-26 22:51:33 +0900

    表示を修正
    package.json修正したので、npm install 必要

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <nav>
-    <!-- <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> -->
-    <router-link to="/community">コミュニティー</router-link>
-    <router-link to="/request">リクエスト</router-link>
+    <router-link to="/community">コミュニティー</router-link>|
+    <router-link to="/request">リクエスト</router-link>|
     <router-link to="/mypage">マイページ</router-link>
   </nav>
-  <router-view/>
+  <div class="d-flex justify-content-center">
+    <router-view/>
+  </div>
 </template>
 
 <style>
